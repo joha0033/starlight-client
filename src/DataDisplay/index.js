9466b2fa14358714b46bc7ad0a25ee996ad0af90
Austin <joha0033@gmail.com>
2018-04-09 13:31:16 -0600

    added DataDisplay Component, and React-Router-DOM

---
+++ src/DataDisplay/index.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+class DataDisplay extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+
+    }
+  }
+
+  render() {
+
+    return (
+      <h1>Data display!</h1>
+    )
+
+
+  }
+}
+
+export default DataDisplay
