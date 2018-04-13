6eb6fa5b724bb8f66976f68fcb1cd2134f364786
Austin <joha0033@gmail.com>
2018-04-13 15:27:29 -0600

    ENV Variable

--- src/DataDisplay/Map/index.js
+++ src/DataDisplay/Map/index.js
@@ -24,7 +24,8 @@ class SimpleMap extends React.Component {
 
        <GoogleMapReact
         google={this.props.google}
-        bootstrapURLKeys={{ key: 'AIzaSyChyTQR99XFwMy--qCA5X5AU4HrkQK_hVw' }}
+        // bootstrapURLKeys={{ key: 'AIzaSyChyTQR99XFwMy--qCA5X5AU4HrkQK_hVw' }}
+        bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_MAPS_KEY }}
         layerTypes={['TrafficLayer']}
         defaultCenter={{
           lat: this.props.lat,
